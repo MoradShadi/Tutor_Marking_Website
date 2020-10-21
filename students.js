8c146096340e13ac9843cc282703c511da685be7
Teo Wei Sheng <wteo0011@student.monash.edu>
2020-10-21 14:36:36 +0800

    Implemented the form for assigning students into groups in js (WIP)

--- students.js
+++ students.js
@@ -40,3 +40,23 @@ function displayStudents(){
 }
 
 displayStudents();
+
+function groupProjectForm(){
+  let output = "";
+  output += "<div style=\"color:black\"><form action=\"#\"><br>"
+  output += "<h4><b>Unit Name:</b></h4><div class=\"mdl-textfield mdl-js-textfield mdl-textfield--floating-label\" style=\"width:100%\">"
+  output += "<select class=\"mdl-textfield__input\" id=\"task\" for=\"task\" onchange=\"selectTask(this)\">"
+  output += "<option value=\"Select\" hidden>Select</option>";
+  output += "<option value='test'>Test</option>"
+  output += "</select></div><br>"
+  output += "<button style=\"margin:auto;width:200px;height:auto;display:block;\" class=\"mdl-button mdl-js-button mdl-button--raised\" onclick=\"addContributions()\">Filter</button>"
+  output += "<h4><b>Project Name:</b></h4><div class=\"mdl-textfield mdl-js-textfield mdl-textfield--floating-label\" style=\"width:100%\">"
+  output += "<select class=\"mdl-textfield__input\" id=\"task\" for=\"task\" onchange=\"selectTask(this)\"></select></div><br>"
+  output += "<h4><b>Students:</b></h4><div class=\"mdl-textfield mdl-js-textfield mdl-textfield--floating-label\" style=\"width:100%\">"
+  output += "<input class=\"mdl-textfield__input\" type=\"number\" id=\"j-hours\" pattern=\"[A-Z,a-z,0-9, ]*\">"
+  output += "<label class=\"mdl-textfield__label\" for=\"j-source\">Hours Contributed</label>"
+  output += "<span class=\"mdl-textfield__error\">Letters, numbers and spaces only</span></div><br></form></div>"
+  document.getElementById("addContributionsForm").innerHTML = output;
+}
+
+groupProjectForm();
